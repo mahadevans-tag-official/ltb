7630a7a62c1ec37fd4c032405d02cffdf5254234
pyratin <pyratin@gmail.com>
2022-10-13 21:43:37 +0530

    done for today

--- source/client/Route/index.js
+++ source/client/Route/index.js
@@ -1,26 +1,43 @@
-import React, { useRef, useCallback, useEffect } from 'react';
-import { css } from '@emotion/react';
+import React, { useRef, useState, useCallback, useEffect } from 'react';
+import $ from 'jquery';
+
+const regionCollection = [
+  'select region',
+  'Europe',
+  'Middle East',
+  'The Americas',
+  'Oceania',
+  'Asia',
+  'Africa'
+];
 
 const Route = () => {
   const ref = useRef();
 
+  const [activeSheet, setActiveSheet] = useState();
+
+  const [, setRegion] = useState(regionCollection[0]);
+
   const vizInitialize = useCallback(() => {
     new globalThis.tableau.Viz(
-      ref.current,
+      $(ref.current).find('.viz').get(0),
       'https://public.tableau.com/views/WorldIndicators/GDPpercapita',
       (() => {
-        const element = /** @type {any} */ (ref.current);
+        const { width, height } = /** @type {any} */ (
+          ref.current
+        ).getBoundingClientRect();
 
         return {
-          width: element.offsetWidth,
-          height: element.offsetHeight,
+          width,
+          height,
           hideTabs: true,
           hideToolbar: true,
-          onFirstInteractive(tableau) {
-            const viz = tableau.getViz();
+          onFirstInteractive(_tableau) {
+            const viz = _tableau.getViz();
             const workbook = viz.getWorkbook();
             const activeSheet = workbook.getActiveSheet();
-            console.log(activeSheet);
+
+            setActiveSheet(activeSheet);
           }
         };
       })()
@@ -32,11 +49,36 @@ const Route = () => {
   }, []);
 
   return (
-    <div
-      ref={ref}
-      css={{ width: '100%', height: '100vh' }}
-      className='Route'
-    ></div>
+    <div ref={ref} className='Route' css={{ width: '100%', height: 700 }}>
+      <div className='w-100 h-100 d-flex flex-column'>
+        <div className='viz'></div>
+
+        <div>
+          <select
+            className='form-select'
+            onChange={(event) => {
+              const region = event.target.value;
+
+              setRegion(region);
+
+              /** @type {any} */ (activeSheet).applyFilterAsync(
+                'Region',
+                region,
+                globalThis.tableau.FilterUpdateType.REPLACE
+              );
+            }}
+          >
+            {regionCollection.map((_region, index) => {
+              return (
+                <option key={index} value={_region}>
+                  {_region}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+      </div>
+    </div>
   );
 };
 
